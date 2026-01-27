b6415ff6b9d9660989b6525de35764b5939563d4
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2026-01-27 14:43:03 -0500

    fix: Write error message to stderr

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bot import Bot
 
 BOT_TOKEN: str | None = os.environ.get('BOT_TOKEN')
 if not BOT_TOKEN:
-    print("[ERROR]: Bot token environment variable is unset! Set in venv/bin/activate")
+    sys.stderr.write('[ERROR]: Bot token environment variable is unset! Set in venv/bin/activate\n')
     sys.exit(1)
     
 
