a0a9b2dcf9a414ee7e601b63a896af1abf1b96da
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2026-01-28 22:31:23 -0500

    style: Use type hinting for bot

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import asyncio
 from bot import Bot
 
 BOT_TOKEN: str | None = os.environ.get('BOT_TOKEN')
+
 if not BOT_TOKEN:
     sys.stderr.write('[ERROR]: Bot token environment variable is unset! Set in venv/bin/activate\n')
     sys.exit(1)
-    
 
-bot = Bot()
+bot: Bot = Bot()
 
 async def main() -> None:
     async with bot:
