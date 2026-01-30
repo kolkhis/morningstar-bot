b03b14d6037976faa29ff414f9bc585d65b22831
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2026-01-29 19:44:55 -0500

    feat(test): Add test slash cmd

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
 import sys
 import asyncio
+import discord
 from bot import Bot
 
-BOT_TOKEN: str | None = os.environ.get('BOT_TOKEN')
+BOT_TOKEN: str = os.environ.get('BOT_TOKEN', 'empty')
 
-if not BOT_TOKEN:
+if BOT_TOKEN == 'empty':
     sys.stderr.write('[ERROR]: Bot token environment variable is unset! Set in venv/bin/activate\n')
     sys.exit(1)
 
 bot: Bot = Bot()
+
+@bot.tree.command(name="test", description="Test command to start")
+async def testcmd(ita: discord.Interaction):
+    await ita.response.send_message("Test command successfully executed")
+    return
 
 async def main() -> None:
     async with bot:
