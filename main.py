5237568e16d6f07744dd0efddb202096b1d389a1
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2026-02-06 19:55:15 -0500

    feat: Add start logic for date

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from discord import app_commands
 from discord.ext import commands, tasks
 from bot import Bot
 
+import datetime as dt
+import locale
+locale.setlocale(locale.LC_TIME, 'C') # use English month names
+
 BOT_TOKEN: str = os.environ.get('BOT_TOKEN', 'empty')
 
 if BOT_TOKEN == 'empty':
@@ -13,6 +17,14 @@ if BOT_TOKEN == 'empty':
     sys.exit(1)
 
 bot: Bot = Bot()
+
+async def title_to_datetime(date: str):
+    """
+    Take in a date and return a datetime object. The strftime format for the forum
+    posts' titles is '%B %d, %Y'. This function will take in the date as a
+    string and return a datetime object using that format.
+    """
+    return dt.datetime.strptime(date, '%B %d, %Y')
 
 @bot.tree.command(name="test", description="Test command to start")
 async def testcmd(ita: discord.Interaction):
@@ -27,18 +39,25 @@ async def testparams(ita: discord.Interaction, hour: int, minute: int):
     return
 
 @bot.tree.command(name="addpost", description="Command to manually add a post to the forum channel")
+@app_commands.describe(title="Title of the forum post", body="The contents of the forum post")
 async def addpost(ita: discord.Interaction, title: str, body: str):
-    print(f"Attempting to add forum post with the title {title} and content {body}")
+    print(f"Attempting to add forum post with the title \"{title}\" and " \
+          f"content:\n {body}")
+    now = dt.datetime.now()
+    title = now.strftime('%B %d, %Y')
     if not title or not body:
         await ita.response.send_message("ERROR: Either a body or title was not provided!")
         return
     post = await bot.add_forum_post(title=title, body=body)
     await ita.response.send_message(f"Thread was successfully created: {post.mention}")
+    return
 
 async def main() -> None:
     async with bot:
         print("Bot starting...")
         await bot.start(BOT_TOKEN)
+        print("Bot is done.")
+        return
 
 if __name__ == '__main__':
     asyncio.run(main())
