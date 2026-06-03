f8513ec4e68f50c852b5acded44817dbb5666606
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2026-06-03 18:45:42 -0400

    chore: Clean up comments

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ locale.setlocale(locale.LC_TIME, 'C') # use English month names
 # The rest of days, guild party will be at 15:00
 # - Guild party can be changed from 19:00 to 15:00 and still run the next day,
 #   but aura injected at the 19:00 party will count towards the subsequent one.
-# - TODO(check): See if party can seamlessly change between 15:00 and 19:00 for
-#   next-day.  
-# Friday, Breaking Army will be at 19:00
-# Friday, Showdown will be at 20:00
 GUILD_EVENTS: dict[str, dict[str, str]] = {
     "Guild Party": {
         "Monday": "19:00",
