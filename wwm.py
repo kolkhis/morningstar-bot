d1f1e55b7de96e4f5a4dc790261c5c3a0a848dfd
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2026-06-09 17:00:56 -0400

    feat: Add method to add wwm uid to table

--- wwm.py
+++ wwm.py
@@ -9,7 +9,7 @@ user_schema="""
 CREATE TABLE IF NOT EXISTS wwm_profiles (
     discord_user_id INTEGER PRIMARY KEY,
     updated_at TEXT NOT NULL
-    uid TEXT NOT NULL,
+    uid INTEGER NOT NULL,
     name TEXT,
     mythic_rank TEXT,
     dps TEXT,
@@ -28,4 +28,19 @@ class WWM(commands.GroupCog, name="wwm"):
         cursor.execute(user_schema)
         self.bot.db.commit()
 
-        
+    def set_uid(self, discord_user_id: int, uid: int):
+        """create or update a user's wwm UID"""
+        cursor = self.bot.db.cursor()
+        cursor.execute(
+            """
+            INSERT INTO wwm_profiles (discord_user_id, uid, updated_at)
+            VALUES (?, ?, ?)
+            ON CONFLICT(user_id) DO UPDATE
+                SET
+                uid = excluded.uid,
+                updated_at = excluded.updated_at
+            """,
+            (discord_user_id, uid, discord.utils.utcnow().isoformat()),
+        )
+        self.bot.db.commit()
+
